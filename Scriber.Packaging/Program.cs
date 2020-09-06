c6705ebfc732f55bcadbf489888d6844d03eeb2a
Mark Sujew <msujew@yahoo.de>
2020-09-07 00:02:44 +0200

    Initial Commit

---
+++ Scriber.Packaging/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Execution;
+using Microsoft.CodeAnalysis.CSharp;
+using Scriber.Packaging.Documentation;
+using Scriber.Packaging.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Scriber.Packaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
